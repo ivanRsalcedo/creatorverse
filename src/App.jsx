9feb5e0db72865761b956f5ec56f97ccdf3c7862
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2026-01-26 19:43:58 -0500

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useRoutes } from "react-router-dom";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import ShowCreators from "./pages/ShowCreators";
+import ViewCreator from "./pages/ViewCreator";
+import EditCreator from "./pages/EditCreator";
+import AddCreator from "./pages/AddCreator";
+
+function App() {
+  const routes = useRoutes([
+    { path: "/", element: <ShowCreators /> },
+    { path: "/new", element: <AddCreator /> },
+    { path: "/creators/:id", element: <ViewCreator /> },
+    { path: "/creators/:id/edit", element: <EditCreator /> },
+  ]);
+
+  return routes;
 }
 
-export default App
+export default App;
